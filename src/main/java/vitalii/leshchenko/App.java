073e7590b548413c8df41c957e21cfa505758259
Leshchenko <411100253g@gmail.com>
2023-05-24 13:59:54 +0300

    intit commit

---
+++ src/main/java/vitalii/leshchenko/App.java
@@ -0,0 +1,13 @@
+package vitalii.leshchenko;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
