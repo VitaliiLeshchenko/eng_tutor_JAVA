7b5dbfa379698b6f5456f57aeea7fb85bf1e6e82
Leshchenko <411100253g@gmail.com>
2023-09-13 13:20:51 +0300

    added word type enum

--- src/main/java/vitalii/leshchenko/entities/WordClass.java
+++ src/main/java/vitalii/leshchenko/entities/WordClass.java
@@ -1,15 +1,16 @@
 package vitalii.leshchenko.entities;
 
 public enum WordClass {
-  noun,
-  adjective,
-  adverb,
-  article,
-  pronoun,
-  verb,
-  preposition,
-  postposition,
-  conjunction,
-  determiner,
-  interjection
+    noun,
+    adjective,
+    adverb,
+    article,
+    pronoun,
+    verb,
+    preposition,
+    postposition,
+    conjunction,
+    determiner,
+    interjection,
+    sentence
 }
